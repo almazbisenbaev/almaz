0a0c4578d720fab72e9397a41c1578e4a735fa43
Almaz Bisenbaev <almaz9527@gmail.com>
2025-12-10 01:37:08 +0500

    Update posts page

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -6,47 +6,41 @@ export default function Posts(){
         <div className="container mx-auto px-6">
 
             <div className="page-header">
-                <h1 className="font-semibold text-2xl mb-8 py-4 tracking-tighter">My publications</h1>
+                <h1 className="font-semibold text-4xl mb-8 py-6 tracking-tighter">My publications</h1>
             </div>
 
             <div className="posts">
 
                 <div>
                     <a
-                        className="flex flex-col space-y-1 mb-4"
+                        className="flex gap-3 mb-4"
                         href="https://www.threads.com/@almazbisenbaev/post/DLKwRKuRv0O"
                         target='_blank'
                     >
-                        <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-                            <div className="text-neutral-600 w-[110px] tabular-nums">June 21, 2025</div>
-                            <div className="text-neutral-900 tracking-tight">How to make your Elementor website win PageSpeed</div>
-                        </div>
+                        <div className="text-neutral-500 w-[120px]">June 21, 2025</div>
+                        <div className="text-neutral-900">How to make your Elementor website win PageSpeed</div>
                     </a>
                 </div>
 
                 <div>
                     <a
-                        className="flex flex-col space-y-1 mb-4"
+                        className="flex gap-3 mb-4"
                         href="https://dev.to/almazbisenbaev/how-to-write-css-in-2025-modern-features-you-should-be-using-with-examples-3g47"
                         target='_blank'
                     >
-                        <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-                            <div className="text-neutral-600 w-[110px] tabular-nums">June 5, 2025</div>
-                            <div className="text-neutral-900 tracking-tight">How to Write CSS in 2025 – Modern Features You Should Be Using (with examples)</div>
-                        </div>
+                        <div className="text-neutral-500 w-[120px]">June 5, 2025</div>
+                        <div className="text-neutral-900">How to Write CSS in 2025 – Modern Features You Should Be Using (with examples)</div>
                     </a>
                 </div>
 
                 <div>
                     <a
-                        className="flex flex-col space-y-1 mb-4"
+                        className="flex gap-3 mb-4"
                         href="https://webdevandstuff.hashnode.dev/mastering-productivity-1"
                         target='_blank'
                     >
-                        <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-                            <div className="text-neutral-600 w-[110px] tabular-nums">April 17, 2025</div>
-                            <div className="text-neutral-900 tracking-tight">Mastering Productivity as a Freelancer</div>
-                        </div>
+                        <div className="text-neutral-500 w-[120px]">April 17, 2025</div>
+                        <div className="text-neutral-900">Mastering Productivity as a Freelancer</div>
                     </a>
                 </div>
 
