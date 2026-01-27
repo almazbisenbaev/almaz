074b1b66e198a78317431baac957370e04cda9d8
Almaz Bisenbaev <almaz9527@gmail.com>
2026-01-27 06:23:05 +0500

    Added 404 page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="min-h-[70vh] container mx-auto px-6 flex flex-col items-start justify-center">
+
+        <h2 className="text-[120px] sm:text-[160px] leading-[1em] font-bold tracking-tight rotate-180">C'est la vie</h2>
+        <p className="text-md sm:text-xl font-medium">Page not found</p>
+
+    </div>
+  );
+}
