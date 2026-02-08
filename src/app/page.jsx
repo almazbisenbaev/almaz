1901239721f574c489399bc0f7e038deeb26002d
Almaz Bisenbaev <almaz9527@gmail.com>
2026-02-09 04:00:32 +0500

    Replace twitter with youtube

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -304,9 +304,9 @@ export default function Home() {
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z" fill="currentColor"></path></svg>
                     Threads
                   </a>
-                  <a target="_blank" href="https://x.com/almazbisenbaev" className="extlink">
+                  <a target="_blank" href="https://www.youtube.com/@webdevandstuff" className="extlink">
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z" fill="currentColor"></path></svg>
-                    Twitter
+                    YouTube
                   </a>
                 </div>
 
