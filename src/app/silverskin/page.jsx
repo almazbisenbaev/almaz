9782cb8a441248ad8a4509da9fbae5d577d3935f
Almaz Bisenbaev <almaz9527@gmail.com>
2026-04-20 19:38:15 +0500

    Add outline for case page

--- src/app/silverskin/page.jsx
+++ src/app/silverskin/page.jsx
@@ -1,6 +1,15 @@
 export default function PageSilverskin(){
     return (
         <>
+
+            <h1>Title</h1>
+            <div>Blah blah website for this company</div>
+            <div>Main screenshot or video</div>
+            <div>Tech stack</div>
+            <div>Screenshots (or carousel)</div>
+
+            <div>Offer (work with me if you want this and htis)</div>
+
         </>
     )
 }
